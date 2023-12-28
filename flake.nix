39524c4ab60ef43fad1dfab80836d453789deaad
Mutsuha Asada <me@momee.mt>
2023-12-28 09:18:09 +0900

    initialized to be reimplemented in Rust

--- flake.nix
+++ flake.nix
@@ -1,25 +1,42 @@
 {
-  description = "A very basic flake";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
-        packages = flake-utils.lib.flattenTree
-          {
-            racco = pkgs.nimPackages.buildNimPackage {
-              name = "racco";
-              src = ./.;
-            };
-          };
-        defaultPackage = packages.racco;
-        apps.racco = flake-utils.lib.mkApp { drv = packages.racco; };
-        defaultApp = apps.racco;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = { self, nixpkgs, fenix, flake-utils, flake-compat, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [
+          (_: super: let pkgs = fenix.inputs.nixpkgs.legacyPackages.${super.system}; in fenix.overlays.default pkgs pkgs)
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs;  [ nim-unwrapped-2 nimPackages.nimble darwin.apple_sdk.frameworks.Security ];
+          buildInputs = with pkgs; [
+            nil
+            openssl
+            glib
+            pkg-config
+            (fenix.packages.${system}.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            rust-analyzer-nightly
+          ];
+          RUST_SRC_PATH = "${fenix.packages.${system}.complete.rust-src}/lib/rustlib/src/rust/library";
         };
       }
     );
--- src/brack.nim
+++
@@ -1,6 +0,0 @@
-import brack/tokenizer
-import brack/parser
-import brack/initialize
-import brack/std
-
-export tokenizer, parser, initialize, std
