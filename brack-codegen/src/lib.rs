1d26fe868a6b2132ae20350bee3caa40a45511fd
Mutsuha Asada <me@momee.mt>
2024-01-18 19:30:02 +0900

    implemented codegen

--- brack-codegen/src/lib.rs
+++ brack-codegen/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod curly;
+mod expr;
+pub mod generate;
+mod identifier;
+mod square;
+mod stmt;
+mod text;
---
+++ brack-codegen/src/stmt.rs
@@ -0,0 +1,22 @@
+use anyhow::Result;
+use brack_parser::ast::AST;
+use brack_plugin::plugin::Plugins;
+
+use crate::{expr, curly, square, identifier, text};
+
+pub fn generate(ast: &AST, plugins: &mut Plugins) -> Result<String> {
+    let mut result = String::from("");
+    for child in ast.children() {
+        let res = match child {
+            AST::Expr(_) => expr::generate(&child, plugins)?,
+            AST::Curly(_) => curly::generate(&child, plugins)?,
+            AST::Square(_) => square::generate(&child, plugins)?,
+            AST::Identifier(_) => identifier::generate(&child)?,
+            AST::Text(_) => text::generate(&child)?,
+            AST::Angle(_) => anyhow::bail!("Angle must be expanded by the macro expander."),
+            _ => anyhow::bail!("Stmt cannot contain Document and Stmt"),
+        };
+        result.push_str(&res);
+    }
+    Ok(result + "\n")
+}
