27a56d4a0e8d6c46d0cd6e1d500f6df3c3a6ec19
Mutsuha Asada <me@momee.mt>
2024-01-26 17:12:05 +0900

    fixed AST matching

--- brack-expander/src/expand.rs
+++ brack-expander/src/expand.rs
@@ -46,6 +46,12 @@ fn expand_angle(overall_ast: &AST, ast: &AST, plugins: &mut Plugins) -> Result<A
 
 fn expand_other(overall_ast: &AST, ast: &AST, plugins: &mut Plugins) -> Result<AST> {
     let mut children = vec![];
+    match ast {
+        AST::Text(_) => {
+            return Ok(ast.clone());
+        },
+        _ => {}
+    }
     for child in ast.children() {
         match child {
             AST::Angle(_) => children.push(expand_angle(overall_ast, &child, plugins)?),
