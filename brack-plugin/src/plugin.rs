64ca61700e19d51e29edb206a47f8b73302f0ec4
Mutsuha Asada <me@momee.mt>
2024-02-12 22:54:43 +0900

    added arg_counter

--- brack-plugin/src/plugin.rs
+++ brack-plugin/src/plugin.rs
@@ -83,6 +83,28 @@ pub fn new_plugins2<P: AsRef<Path>>(pathes: Vec<P>) -> Result<Plugins2> {
     Ok(result)
 }
 
+pub fn arg_counter(arg_types: &Vec<Type>) -> (usize, usize) {
+    let mut min = 0;
+    let mut max = 0;
+    for arg_type in arg_types {
+        match arg_type {
+            Type::TOption(_) => {
+                min += 0;
+                max += 1;
+            }
+            Type::TArray(_) => {
+                min += 0;
+                max = usize::MAX;
+            }
+            _ => {
+                min += 1;
+                max += 1;
+            }
+        }
+    }
+    (min, max)
+}
+
 #[cfg(test)]
 mod tests {
     use core::panic;
