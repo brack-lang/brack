0e7307cc3243b4cb30fe9cc3296f46436b1c64e9
Mutsuha Asada <me@momee.mt>
2024-08-26 17:58:36 +0900

    delete: unneccesary parser component

--- brack-parser/src/debug/debug_compile.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-
-static FILE_PATH: &str = "sample-text/no_commands.[]";
-
-fn main() {
-    let tokens = brack_tokenizer::tokenize::tokenize(&FILE_PATH).unwrap();
-    let ast = brack_parser::parse::parse(&tokens).unwrap();
-    dbg!(ast);
-}
