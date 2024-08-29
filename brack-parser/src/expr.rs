1bae2b5162c7768592cb4d2cec2bebe4b1a4b456
Mutsuha Asada <me@momee.mt>
2024-08-29 11:57:53 +0900

    change: expr syntax to make transformation easier

--- brack-parser/src/expr.rs
+++ brack-parser/src/expr.rs
@@ -5,41 +5,37 @@ use crate::{
     bracket, comma, cst::new_expr, dot, escaped, ident, modules, parser::Parser, text, whitespace,
 };
 
-// (escaped | module | ident | bracket | dot | comma | whitespace | text)+
+// escaped | module | ident | bracket | dot | comma | whitespace | text
 pub fn parse<'a>(tokens: &'a [Token]) -> Result<Parser> {
     let mut tokens = tokens;
     let mut expr = new_expr();
 
-    loop {
-        if let Ok((csts, new_tokens)) = escaped::parse(tokens) {
-            for cst in csts {
-                expr.add(cst);
-            }
-            tokens = new_tokens;
-        } else if let Ok((cst, new_tokens)) = modules::parse(tokens) {
+    if let Ok((csts, new_tokens)) = escaped::parse(tokens) {
+        for cst in csts {
             expr.add(cst);
-            tokens = new_tokens;
-        } else if let Ok((cst, new_tokens)) = ident::parse(tokens) {
-            expr.add(cst);
-            tokens = new_tokens;
-        } else if let Ok((cst, new_tokens)) = bracket::parse(tokens) {
-            expr.add(cst);
-            tokens = new_tokens;
-        } else if let Ok((cst, new_tokens)) = dot::parse(tokens) {
-            expr.add(cst);
-            tokens = new_tokens;
-        } else if let Ok((cst, new_tokens)) = comma::parse(tokens) {
-            expr.add(cst);
-            tokens = new_tokens;
-        } else if let Ok((cst, new_tokens)) = whitespace::parse(tokens) {
-            expr.add(cst);
-            tokens = new_tokens;
-        } else if let Ok((cst, new_tokens)) = text::parse(tokens) {
-            expr.add(cst);
-            tokens = new_tokens;
-        } else {
-            break;
         }
+        tokens = new_tokens;
+    } else if let Ok((cst, new_tokens)) = modules::parse(tokens) {
+        expr.add(cst);
+        tokens = new_tokens;
+    } else if let Ok((cst, new_tokens)) = ident::parse(tokens) {
+        expr.add(cst);
+        tokens = new_tokens;
+    } else if let Ok((cst, new_tokens)) = bracket::parse(tokens) {
+        expr.add(cst);
+        tokens = new_tokens;
+    } else if let Ok((cst, new_tokens)) = dot::parse(tokens) {
+        expr.add(cst);
+        tokens = new_tokens;
+    } else if let Ok((cst, new_tokens)) = comma::parse(tokens) {
+        expr.add(cst);
+        tokens = new_tokens;
+    } else if let Ok((cst, new_tokens)) = whitespace::parse(tokens) {
+        expr.add(cst);
+        tokens = new_tokens;
+    } else if let Ok((cst, new_tokens)) = text::parse(tokens) {
+        expr.add(cst);
+        tokens = new_tokens;
     }
 
     if expr.children().is_empty() {
