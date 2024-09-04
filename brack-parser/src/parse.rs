f784923b5714217f7df3dc0205befb32d166e717
Mutsuha Asada <me@momee.mt>
2024-09-04 13:08:18 +0900

    fix: parse failure for document node

--- brack-parser/src/parse.rs
+++ brack-parser/src/parse.rs
@@ -12,32 +12,42 @@ pub fn parse<'a>(tokens: &'a [Token]) -> Result<CST> {
     let mut cst = new_document();
 
     loop {
-        if let Ok((cst1, new_tokens)) = stmt::parse(tokens) {
-            cst.add(cst1);
-            tokens = new_tokens;
+        let mut csts = vec![];
+        let mut tokens1 = tokens;
+
+        if let Ok((cst1, new_tokens)) = stmt::parse(tokens1) {
+            tokens1 = new_tokens;
+            csts.push(cst1);
         } else {
             break;
         }
 
-        if let Ok((cst2, new_tokens)) = newline::parse(tokens) {
-            cst.add(cst2);
-            tokens = new_tokens;
+        if let Ok((cst2, new_tokens)) = newline::parse(tokens1) {
+            tokens1 = new_tokens;
+            csts.push(cst2);
         } else {
             break;
         }
 
         loop {
-            if let Ok((cst3, new_tokens)) = newline::parse(tokens) {
-                cst.add(cst3);
-                tokens = new_tokens;
+            if let Ok((cst3, new_tokens)) = newline::parse(tokens1) {
+                tokens1 = new_tokens;
+                csts.push(cst3);
             } else {
                 break;
             }
         }
+
+        tokens = tokens1;
+        for cst1 in csts {
+            cst.add(cst1);
+        }
     }
 
-    let (cst1, mut tokens) = stmt::parse(tokens)?;
-    cst.add(cst1);
+    if let Ok((cst1, new_tokens)) = stmt::parse(tokens) {
+        cst.add(cst1);
+        tokens = new_tokens;
+    }
 
     loop {
         if let Ok((cst1, new_tokens)) = newline::parse(tokens) {
