7169dec80c7e68b32b1012690c536f649af7c28c
Mutsuha Asada <me@momee.mt>
2024-10-08 16:08:38 +0900

    add: plugin metadata, type, and value

--- brack-plugin/src/plugin.rs
+++ brack-plugin/src/plugin.rs
@@ -1,14 +1,40 @@
 use std::{collections::HashMap, fs, path::Path};
 
 use anyhow::Result;
-use brack_sdk_rs::{MetaData, Type};
 use extism::Plugin;
 use extism_convert::Json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash)]
+pub enum Type {
+    TInline,
+    TOption(Box<Type>),
+    TBlock,
+    TArray(Box<Type>),
+    TInlineCmd(String),
+    TBlockCmd(String),
+    TAST,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Metadata {
+    pub command_name: String,
+    pub call_name: String,
+    pub argument_types: Vec<(String, Type)>,
+    pub return_type: Type,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum Value {
+    Text(String),
+    TextArray(Vec<String>),
+    TextOption(Option<String>),
+}
 
 pub type ModuleName = String;
 pub type CommandName = String;
 pub type Plugins = HashMap<ModuleName, (Plugin, PluginMetaDataMap)>;
-pub type PluginMetaDataMap = HashMap<(CommandName, Type), MetaData>;
+pub type PluginMetaDataMap = HashMap<(CommandName, Type), Metadata>;
 
 pub fn new_plugins<M: AsRef<str>, P: AsRef<Path>>(
     plutin_path_map: HashMap<M, P>,
@@ -18,7 +44,7 @@ pub fn new_plugins<M: AsRef<str>, P: AsRef<Path>>(
         let name = name.as_ref().to_string();
         let path = path.as_ref().to_path_buf();
         let mut extism_plugin = Plugin::new(fs::read(&path)?, [], true)?;
-        let Json(metadatas) = extism_plugin.call::<(), Json<Vec<MetaData>>>("get_metadata", ())?;
+        let Json(metadatas) = extism_plugin.call::<(), Json<Vec<Metadata>>>("get_metadata", ())?;
         let mut metadata_map = HashMap::new();
         for metadata in metadatas {
             metadata_map.insert(
