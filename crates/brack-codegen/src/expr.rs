a58b4f220424a5b18fec98dcb436c27fe6346ea2
Mutsuha Asada <me@momee.mt>
2024-10-23 14:41:59 +0900

    fix: apply pointing out by Clippy on brack-codegen

--- crates/brack-codegen/src/expr.rs
+++ crates/brack-codegen/src/expr.rs
@@ -12,11 +12,11 @@ pub(crate) fn generate(ast: &AST, plugins: &mut Plugins) -> Result<String> {
     let mut result = String::from("");
     for child in ast.children() {
         let res = match child {
-            AST::Curly(_) => curly::generate(&child, plugins)?,
-            AST::Square(_) => square::generate(&child, plugins)?,
-            AST::Text(_) => text::generate(&child, plugins)?,
+            AST::Curly(_) => curly::generate(child, plugins)?,
+            AST::Square(_) => square::generate(child, plugins)?,
+            AST::Text(_) => text::generate(child, plugins)?,
             AST::Angle(_) => anyhow::bail!("Angle must be expanded by the macro expander."),
-            AST::Expr(_) => generate(&child, plugins)?,
+            AST::Expr(_) => generate(child, plugins)?,
             ast => anyhow::bail!("Expr cannot contain the following node\n{}", ast),
         };
         result.push_str(&res);
--- crates/brack-codegen/src/square.rs
+++ crates/brack-codegen/src/square.rs
@@ -16,18 +16,18 @@ pub(crate) fn generate(ast: &AST, plugins: &mut Plugins) -> Result<String> {
     let mut arguments = vec![];
     let module = ast
         .children()
-        .get(0)
+        .first()
         .ok_or_else(|| anyhow::anyhow!("Square must contain module and identifier"))?;
     let ident = ast
         .children()
         .get(1)
         .ok_or_else(|| anyhow::anyhow!("Square must contain module and identifier"))?;
-    for (_, child) in ast.children().iter().skip(2).enumerate() {
+    for child in ast.children().iter().skip(2) {
         let res = match child {
-            AST::Expr(_) => expr::generate(&child, plugins)?,
-            AST::Curly(_) => curly::generate(&child, plugins)?,
-            AST::Square(_) => generate(&child, plugins)?,
-            AST::Text(_) => text::generate(&child, plugins)?,
+            AST::Expr(_) => expr::generate(child, plugins)?,
+            AST::Curly(_) => curly::generate(child, plugins)?,
+            AST::Square(_) => generate(child, plugins)?,
+            AST::Text(_) => text::generate(child, plugins)?,
             AST::Angle(_) => anyhow::bail!("Angle must be expanded by the macro expander."),
             ast => anyhow::bail!("Square cannot contain the following node\n{}", ast),
         };
@@ -81,7 +81,7 @@ pub(crate) fn generate(ast: &AST, plugins: &mut Plugins) -> Result<String> {
                     Value::TextOption(None)
                 }
             }
-            Type::TArray(_) => Value::TextArray(arguments[i..].iter().map(|s| s.clone()).collect()),
+            Type::TArray(_) => Value::TextArray(arguments[i..].to_vec()),
             _ => Value::Text(arguments[i].clone()),
         };
         args.push(arg);
