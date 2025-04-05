324504b59c5f0067da70f5f35e14a368ece71a79
Mutsuha Asada <me@momee.mt>
2025-04-05 12:39:22 +0900

    fix: into brack_common::errors

--- crates/brack-codegen/src/square.rs
+++ crates/brack-codegen/src/square.rs
@@ -4,7 +4,7 @@ use brack_plugin::{
     types::{arg_counter, Type},
     value::Value,
 };
-use brack_transformer::ast::AST;
+use brack_common::ast::AST;
 
 use crate::{curly, expr, text};
 
@@ -40,7 +40,7 @@ pub(crate) fn generate(ast: &AST, plugins: &mut Plugins) -> Result<String> {
     };
     let module_name = match module_name {
         Some(module_name) => module_name,
-        None => anyhow::bail!("Module name must be a string"),
+        _ => anyhow::bail!("Module name must be a string"),
     };
 
     let ident_name = match ident {
@@ -49,7 +49,7 @@ pub(crate) fn generate(ast: &AST, plugins: &mut Plugins) -> Result<String> {
     };
     let ident_name = match ident_name {
         Some(ident_name) => ident_name,
-        None => anyhow::bail!("Identifier name must be a string"),
+        _ => anyhow::bail!("Identifier name must be a string"),
     };
 
     let arg_types = plugins.argument_types(&module_name, &ident_name, Type::TInline)?;
